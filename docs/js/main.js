403d2949304d21c3af5fc3f89fdbb392c532240d
MCF512 <egdem4enko@yandex.ru>
2021-11-09 21:34:44 +0300

    added feedback and upgrade sections

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
 
 });
+
+$('.feedback__slider').slick({
+  centerMode: true,
+  slidesToShow: 3,
+  // centerPadding: '60px',
+});
+
+$('.feedback__reviews').slick({
+  slidesToShow: 2,
+  arrows: false,
+  dots: true
+})
